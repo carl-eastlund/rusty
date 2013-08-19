a7a63433de47003c1a28769762ad851f3f0d71af
Carl Eastlund <cce@racket-lang.org>
2013-08-18 20:36:43 -0400

    Got vector initialization to compile one way.

--- list.rs
+++ list.rs
@@ -1,16 +1,17 @@
 
 #[deriving (Clone,ToStr)]
-enum List<E> { Null, Cons( E, @List<E> ) }
+enum List<E> { Empty, Cons( @E, @List<E> ) }
 
-fn null<E:'static>() -> List<E> { Null }
-fn cons<E:'static>( x : E, xs : List<E> ) -> List<E> { Cons( x, @xs ) }
+fn is_empty<E>( xs:List<E> ) -> bool { match xs { Empty => true, _ => false } }
+fn empty<E>() -> List<E> { Empty }
+fn cons<E:'static>( x:E, xs:List<E> ) -> List<E> { Cons( @x, @xs ) }
 
 fn foldl<E:Clone+'static,R>( f:&fn(E,R)->R, mut r:R, mut xs:List<E> ) -> R {
     loop {
         match xs {
-            Null => { return r }
+            Empty => { return r }
             Cons( e, ys ) => {
-                r = f(e, r);
+                r = f((*e).clone(), r);
                 xs = (*ys).clone();
                 loop
             }
@@ -23,7 +24,7 @@ fn revapp<E:Clone+'static>( xs:List<E>, ys:List<E> ) -> List<E> {
 }
 
 fn reverse<E:Clone+'static>( xs:List<E> ) -> List<E> {
-    revapp( xs, null() )
+    revapp( xs, empty() )
 }
 
 fn foldr<E:Clone+'static,R>( f:&fn(E,R)->R, v:R, xs:List<E> ) -> R {
@@ -35,11 +36,11 @@ fn append<E:Clone+'static>( xs:List<E>, ys:List<E> ) -> List<E> {
 }
 
 fn append_lists<E:Clone+'static>( xss:List<List<E>> ) -> List<E> {
-    foldr( append, null(), xss )
+    foldr( append, empty(), xss )
 }
 
 fn map<A:Clone+'static,B:'static>( f:&fn(A)->B, xs:List<A> ) -> List<B> {
-    foldr( |x,ys| cons(f(x),ys), null(), xs )
+    foldr( |x,ys| cons(f(x),ys), empty(), xs )
 }
 
 trait Discrim<K,V> {
@@ -48,22 +49,25 @@ trait Discrim<K,V> {
 
 impl<V:Clone+'static> Discrim<u8,V> for () {
     fn discrim( &self, mut xs:List<(u8,V)> ) -> List<List<V>> {
-        let mut vec = [Null, ..256];
+        let mut vec = [Empty, ..256];
         loop {
             match xs {
-                Null => {}
-                Cons( (k,v), ys ) => {
-                    vec[k] = cons(v,vec[k]);
-                    xs = *ys;
-                    loop
+                Empty => { break }
+                Cons( e, ys ) => {
+                    match (*e).clone() {
+                        (k,v) => {
+                            vec[k] = cons(v,vec[k].clone());
+                            xs = (*ys).clone();
+                            loop
+                        }
+                    }
                 }
             }
         }
-        let mut vss = Null;
+        let mut vss = Empty;
         for i in vec.rev_iter() {
-            match *i {
-                Null => {}
-                vs => { vss = cons(reverse(vs), vss) }
+            if( !is_empty(*i) ) {
+                vss = cons(reverse(*i), vss);
             }
         }
         vss
@@ -75,11 +79,12 @@ fn dsort<V:Clone+'static>( d:@Discrim<V,V>, xs:List<V> ) -> List<V> {
 }
 
 fn main () {
-    let fwd : List<u8> = cons(1,cons(2,null()));
+    let fwd : List<u8> = cons(1,cons(2,empty()));
     println(fwd.to_str());
     let rev : List<u8> = reverse(fwd);
     println(rev.to_str());
     let add : List<u8> = map(|x|x-1,rev);
     println(add.to_str());
-    let ord : List<u8> = dsort((),add);
+    // let ord : List<u8> = dsort((),add);
+    // println(ord.to_str());
 }
