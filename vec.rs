de7b6bb2580134ff003361d7096958765674015d
Carl Eastlund <cce@racket-lang.org>
2013-08-19 16:52:41 -0400

    implemented u8 discriminator

--- vec.rs
+++ vec.rs
@@ -21,6 +21,19 @@ fn dsort<T:Clone>( disc : Discrim<T,T>, xs : ~[T] ) -> ~[T] {
     sorted
 }
 
+fn u8_discrim<T>( pairs : ~[(u8,T)] ) -> ~[~[T]] {
+    let mut vec = do vec::build_sized(256) |push| {
+        for _ in range(0,256) {
+            push(~[]);
+        }
+    };
+    for ( k, v ) in pairs.move_iter() {
+        vec[k].push(v);
+    }
+    do vec.retain |vs| { vs.len() > 0 };
+    vec
+}
+
 fn main () {
     let two_pi = [6u8,2u8,8u8,3u8,1u8];
     println(two_pi.to_str());
