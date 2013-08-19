fc1fb3bc66bb97742d98a60e596b01a7a3a60e87
Carl Eastlund <cce@racket-lang.org>
2013-08-19 00:37:55 -0400

    minimal bug

--- bug.rs
+++ bug.rs
@@ -1,11 +1,4 @@
 
-#[deriving (Clone,ToStr)]
-enum Type<T> { Empty, Cons( @T, @Type<T> ) }
-
-type Fun<'self,T> = &'self fn(Type<T>) -> Type<Type<T>>;
-
-fn op<'self,T:Clone+'static>( _:Fun<'self,T>, _:Type<T> ) -> Type<T> {
-    Empty
-}
+fn op<'self>( _:&'self fn() -> () ) {}
 
 fn main () {}
