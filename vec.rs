2e88803aa056dcfde93120bfc03d006f37a5a07f
Carl Eastlund <cce@racket-lang.org>
2013-08-19 16:38:56 -0400

    vector discriminators -- got something for dsort that typechecks.

--- vec.rs
+++ vec.rs
@@ -1,20 +1,20 @@
 
 use std::vec;
 
-type Discrim<'self,K,V> = &'self fn( ~[(K,V)] ) -> ~[~[V]];
+type Discrim<K,V> = &'static fn( ~[(K,V)] ) -> ~[~[V]];
 
-fn dsort<'self,T>( disc : Discrim<'self,T,T>, xs : ~[T] ) -> ~[T] {
+fn dsort<T:Clone>( disc : Discrim<T,T>, xs : ~[T] ) -> ~[T] {
     let n = xs.len();
     let pairs = do vec::build_sized(n) |push| {
         for x in xs.iter() {
-            push((x,x));
+            push((x.clone(),x.clone()));
         }
     };
     let groups = disc(pairs);
     let sorted = do vec::build_sized(n) |push| {
         for vs in groups.iter() {
             for v in vs.iter() {
-                push(v);
+                push(v.clone());
             }
         }
     };
