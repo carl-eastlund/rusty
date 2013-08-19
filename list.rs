075c483860661ca5f6ad76bd9b3119904d590de8
Carl Eastlund <cce@racket-lang.org>
2013-08-19 04:40:24 -0400

    working so far

--- list.rs
+++ list.rs
@@ -20,6 +20,13 @@ impl<E> List<E> {
         return r
     }
 
+    fn map<F:'static>( &self, f : &fn(@E)->@F ) -> List<F> {
+        let rev = do self.foldl(Empty) |x,ys| {
+            Cons(f(x),@ys)
+        };
+        rev.reverse()
+    }
+
 }
 
 impl<E:'static> List<E> {
@@ -28,6 +35,39 @@ impl<E:'static> List<E> {
         do self.foldl(xs) |x,ys| {
             Cons(x,@ys)
         }
+    }
+
+    fn reverse( &self ) -> List<E> {
+        self.revapp(Empty)
+    }
+
+    fn foldr<R>( &self, r : R, f : &fn(@E,R)->R ) -> R {
+        self.reverse().foldl(r,f)
+    }
+
+    fn append( &self, xs : List<E> ) -> List<E> {
+        self.reverse().revapp(xs)
+    }
+
+}
+
+impl<E:'static> List<List<E>> {
+
+    fn append_all( &self ) -> List<E> {
+        do self.foldr(Empty) |xs,ys| {
+            xs.append(ys)
+        }
+    }
+
+}
+
+type Discrim<'self,K,V> = &'self fn( List<(K,V)> ) -> List<List<V>>;
+
+impl<'self, E:Clone+'static> List<E> {
+
+    fn dsort( &self, d : Discrim<'self,E,E> ) -> List<E> {
+        let pairs = do self.map |x| { @((*x).clone(),(*x).clone()) };
+        d(pairs).append_all()
     }
 
 }
