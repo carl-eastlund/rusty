1372c680c8f7b830eedbf6cd6a4ba1b7ef1379b6
Carl Eastlund <cce@racket-lang.org>
2013-08-23 17:23:28 -0400

    names

--- trait.rs
+++ trait.rs
@@ -42,32 +42,32 @@ fn vec_partition_move<T>( xs : ~[T], f : &fn(&T)->bool ) -> (~[T],~[T]) {
     (pass,fail)
 }
 
-trait Discrim<K,V> {
+trait Disc<K,V> {
 
-    fn discrim( &self, ~[(K,V)] ) -> ~[~[V]];
+    fn disc( &self, ~[(K,V)] ) -> ~[~[V]];
 
 }
 
-fn discrim_sort<T:Clone,D:Discrim<T,T>>( d : D, xs : &[T] ) -> ~[T] {
+fn disc_sort<T:Clone,D:Disc<T,T>>( d : D, xs : &[T] ) -> ~[T] {
     let pairs = do vec_map(xs) |x| { ((*x).clone(), (*x).clone()) };
-    let groups = d.discrim(pairs);
+    let groups = d.disc(pairs);
     vec_collapse_move( groups )
 }
 
-struct Pair_Discrim<A,B>(A,B);
+struct Pair_Disc<A,B>(A,B);
 
-impl<K1,K2,V,D1:Discrim<K1,(K2,V)>,D2:Discrim<K2,V>>
-Discrim<(K1,K2),V> for Pair_Discrim<D1,D2> {
+impl<K1,K2,V,D1:Disc<K1,(K2,V)>,D2:Disc<K2,V>>
+Disc<(K1,K2),V> for Pair_Disc<D1,D2> {
 
-    fn discrim( &self, pairs : ~[((K1,K2),V)] ) -> ~[~[V]] {
+    fn disc( &self, pairs : ~[((K1,K2),V)] ) -> ~[~[V]] {
         match self {
-            &Pair_Discrim(ref first, ref second) => {
+            &Pair_Disc(ref first, ref second) => {
                 let nested = do vec_map_move(pairs) |((k1,k2),v)| {
                     (k1,(k2,v))
                 };
-                let pass1 = first.discrim(nested);
+                let pass1 = first.disc(nested);
                 let pass2 = do vec_map_move(pass1) |group| {
-                    second.discrim(group)
+                    second.disc(group)
                 };
                 vec_collapse_move( pass2 )
             }
@@ -76,13 +76,13 @@ Discrim<(K1,K2),V> for Pair_Discrim<D1,D2> {
 
 }
 
-struct Map_Discrim<'self,A,B,D>{ key : &'self fn(A)->B, discrim : D }
+struct Map_Disc<'self,A,B,D>{ key : &'self fn(A)->B, disc : D }
 
-impl<'self,A,B,V,D:Discrim<B,V>> Discrim<A,V> for Map_Discrim<'self,A,B,D> {
+impl<'self,A,B,V,D:Disc<B,V>> Disc<A,V> for Map_Disc<'self,A,B,D> {
 
-    fn discrim( &self, pairs : ~[(A,V)] ) -> ~[~[V]] {
+    fn disc( &self, pairs : ~[(A,V)] ) -> ~[~[V]] {
         let mapped = do vec_map_move(pairs) |(k,v)| { ((self.key)(k),v) };
-        self.discrim.discrim(mapped)
+        self.disc.disc(mapped)
     }
 
 }
@@ -96,60 +96,60 @@ macro_rules! make_uint_split {
     }
 }
 
-macro_rules! make_uint_discrim {
+macro_rules! make_uint_disc {
     ($name:ident,$big:ident,$help:ident,$lil:ident,$factor:expr) => {
-        impl<T> Discrim<$big,T> for $name {
-            fn discrim( &self, pairs : ~[($big,T)] ) -> ~[~[T]] {
-                Map_Discrim{
+        impl<T> Disc<$big,T> for $name {
+            fn disc( &self, pairs : ~[($big,T)] ) -> ~[~[T]] {
+                Map_Disc{
                     key: |x:$big| ((x/$factor) as $lil, (x%$factor) as $lil),
-                    discrim: Pair_Discrim($help,$help)
-                }.discrim(pairs)
+                    disc: Pair_Disc($help,$help)
+                }.disc(pairs)
             }
         }
     }
 }
 
-macro_rules! make_int_discrim {
+macro_rules! make_int_disc {
     ($name:ident,$i:ident,$help:ident,$u:ident) => {
-        impl<T> Discrim<$i,T> for $name {
-            fn discrim( &self, pairs : ~[($i,T)] ) -> ~[~[T]] {
-                Map_Discrim{
+        impl<T> Disc<$i,T> for $name {
+            fn disc( &self, pairs : ~[($i,T)] ) -> ~[~[T]] {
+                Map_Disc{
                     key: |x:$i| (x - $i::min_value) as $u,
-                    discrim: $help
-                }.discrim(pairs)
+                    disc: $help
+                }.disc(pairs)
             }
         }
     }
 }
 
-macro_rules! make_cast_discrim {
+macro_rules! make_cast_disc {
     ($name:ident,$from:ident,$help:ident,$to:ident) => {
-        impl<T> Discrim<$from,T> for $name {
-            fn discrim( &self, pairs : ~[($from,T)] ) -> ~[~[T]] {
-                Map_Discrim{
+        impl<T> Disc<$from,T> for $name {
+            fn disc( &self, pairs : ~[($from,T)] ) -> ~[~[T]] {
+                Map_Disc{
                     key: |x:$from| x as $to,
-                    discrim: $help
-                }.discrim(pairs)
+                    disc: $help
+                }.disc(pairs)
             }
         }
     }
 }
 
-struct U8_Discrim;
-struct U16_Discrim;
-struct U32_Discrim;
-struct U64_Discrim;
-struct UInt_Discrim;
+struct U8_Disc;
+struct U16_Disc;
+struct U32_Disc;
+struct U64_Disc;
+struct UInt_Disc;
 
-struct I8_Discrim;
-struct I16_Discrim;
-struct I32_Discrim;
-struct I64_Discrim;
-struct Int_Discrim;
+struct I8_Disc;
+struct I16_Disc;
+struct I32_Disc;
+struct I64_Disc;
+struct Int_Disc;
 
-impl<T> Discrim<u8,T> for U8_Discrim {
+impl<T> Disc<u8,T> for U8_Disc {
 
-    fn discrim( &self, pairs : ~[(u8,T)] ) -> ~[~[T]] {
+    fn disc( &self, pairs : ~[(u8,T)] ) -> ~[~[T]] {
         let mut buckets = do vec::build_sized(256) |push| {
             for _ in range(0,256) {
                 push(~[]);
@@ -164,24 +164,24 @@ impl<T> Discrim<u8,T> for U8_Discrim {
 
 }
 
-make_uint_discrim!(U16_Discrim,u16,U8_Discrim,u8,1u16<<8)
-make_uint_discrim!(U32_Discrim,u32,U16_Discrim,u16,1u32<<16)
-make_uint_discrim!(U64_Discrim,u64,U32_Discrim,u32,1u64<<32)
+make_uint_disc!(U16_Disc,u16,U8_Disc,u8,1u16<<8)
+make_uint_disc!(U32_Disc,u32,U16_Disc,u16,1u32<<16)
+make_uint_disc!(U64_Disc,u64,U32_Disc,u32,1u64<<32)
 
-make_int_discrim!(I8_Discrim,i8,U8_Discrim,u8)
-make_int_discrim!(I16_Discrim,i16,U16_Discrim,u16)
-make_int_discrim!(I32_Discrim,i32,U32_Discrim,u32)
-make_int_discrim!(I64_Discrim,i64,U64_Discrim,u64)
+make_int_disc!(I8_Disc,i8,U8_Disc,u8)
+make_int_disc!(I16_Disc,i16,U16_Disc,u16)
+make_int_disc!(I32_Disc,i32,U32_Disc,u32)
+make_int_disc!(I64_Disc,i64,U64_Disc,u64)
 
-make_cast_discrim!(Int_Discrim,int,I64_Discrim,i64)
-make_cast_discrim!(UInt_Discrim,uint,U64_Discrim,u64)
+make_cast_disc!(Int_Disc,int,I64_Disc,i64)
+make_cast_disc!(UInt_Disc,uint,U64_Disc,u64)
 
-struct Iterator_Discrim<D>{ elem: D }
+struct Iter_Disc<D>{ elem: D }
 
-impl<K,V,I:Iterator<K>,D:Discrim<K,(I,V)>>
-Discrim<I,V> for Iterator_Discrim<D> {
+impl<K,V,I:Iterator<K>,D:Disc<K,(I,V)>>
+Disc<I,V> for Iter_Disc<D> {
 
-    fn discrim( &self, pairs : ~[(I,V)] ) -> ~[~[V]] {
+    fn disc( &self, pairs : ~[(I,V)] ) -> ~[~[V]] {
         let mut done = ~[];
         let mut todo = ~[pairs];
         while( todo.len() > 0 ) {
@@ -199,7 +199,7 @@ Discrim<I,V> for Iterator_Discrim<D> {
                 if( done_group.len() > 0 ) {
                     done.push(done_group);
                 }
-                let todo_groups = self.elem.discrim(work_group);
+                let todo_groups = self.elem.disc(work_group);
                 new_todo.push_all_move( todo_groups );
             }
             todo = new_todo;
@@ -209,24 +209,24 @@ Discrim<I,V> for Iterator_Discrim<D> {
     
 }
 
-impl<'self,K,V,D:Discrim<K,V>> Discrim<K,V> for &'self D {
+impl<'self,K,V,D:Disc<K,V>> Disc<K,V> for &'self D {
 
-    fn discrim( &self, pairs : ~[(K,V)] ) -> ~[~[V]] {
-        (**self).discrim(pairs)
+    fn disc( &self, pairs : ~[(K,V)] ) -> ~[~[V]] {
+        (**self).disc(pairs)
     }
 
 }
 
-struct Vector_Discrim<D>{ elem: D }
+struct Owned_Vec_Disc<D>{ elem: D }
 
-impl<K:Clone,V,D:Discrim<K,(vec::MoveIterator<K>,V)>>
-Discrim<~[K],V> for Vector_Discrim<D> {
+impl<K:Clone,V,D:Disc<K,(vec::MoveIterator<K>,V)>>
+Disc<~[K],V> for Owned_Vec_Disc<D> {
 
-    fn discrim( &self, pairs : ~[(~[K],V)] ) -> ~[~[V]] {
-        let iter_disc = Iterator_Discrim{ elem: &self.elem };
+    fn disc( &self, pairs : ~[(~[K],V)] ) -> ~[~[V]] {
+        let iter_disc = Iter_Disc{ elem: &self.elem };
         let vec_iter = |v:~[K]| v.move_iter();
-        let vec_disc = Map_Discrim{ key: vec_iter, discrim: iter_disc };
-        vec_disc.discrim(pairs)
+        let vec_disc = Map_Disc{ key: vec_iter, disc: iter_disc };
+        vec_disc.disc(pairs)
     }
 
 }
@@ -234,7 +234,7 @@ Discrim<~[K],V> for Vector_Discrim<D> {
 fn main () {
     let input = ~[~[3,1,4], ~[], ~[3,1,4,1,5], ~[1,2,3]];
     println( input.to_str() );
-    let output = discrim_sort( Vector_Discrim{ elem: Int_Discrim }, input );
+    let output = disc_sort( Owned_Vec_Disc{ elem: Int_Disc }, input );
     println( input.to_str() );
     println( output.to_str() );
 }
