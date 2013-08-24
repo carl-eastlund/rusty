f2600197cf664c8f2f237fc98fc9ac146176aa6e
Carl Eastlund <cce@racket-lang.org>
2013-08-23 22:29:34 -0400

    a lot smaller now

--- slow-compile.rs
+++ slow-compile.rs
@@ -1,37 +1,17 @@
 
 enum Tree { Node(~[Tree]) }
 
-struct TreeDisc;
+struct MacGuffin;
 
-impl<T> TreeDisc {
+impl<T> MacGuffin {
 
-    fn disc( &self, pairs : ~[(Tree,T)] ) {
-        let mut ys = ~[];
-        for (Node(k),v) in pairs.move_iter() {
-            ys.push( (k.move_iter(),v) );
-        }
-        let mut sorted = ~[];
-        let mut todo_stack = ~[ys];
-        while( todo_stack.len() > 0 ) {
-            let todo_iters = todo_stack.pop();
-            let mut done = ~[];
-            let mut todo_elems = ~[];
-            for (k,v) in todo_iters.move_iter() {
-                let mut i = k;
-                match i.next() {
-                    None => { done.push(v) }
-                    Some(e) => { todo_elems.push( (e,(i,v)) ) }
-                }
-            }
-            if( done.len() > 0 ) {
-                sorted.push( done );
-            }
-            TreeDisc.disc( todo_elems );
-        }
+    fn disc( &self, _ : ~[(Tree,T)] ) {
+        let todo_elems : ~[(Tree,(Option<Tree>,T))] = ~[];
+        MacGuffin.disc( todo_elems );
     }
 
 }
 
 fn main () {
-    let input : ~[(Tree,())] = ~[]; TreeDisc.disc(input);
+    let input : ~[(Tree,())] = ~[]; MacGuffin.disc(input);
 }
