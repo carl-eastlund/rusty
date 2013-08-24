23f32302b045d4fe15763c28474e415aed9a4cb5
Carl Eastlund <cce@racket-lang.org>
2013-08-23 22:42:34 -0400

    Added comments for bug report.

--- slow-compile.rs
+++ slow-compile.rs
@@ -1,12 +1,18 @@
 
+// If the definition of Data is simpler, the compiler attempts to inline
+// generic<T>, fails, and produces an error.
 struct Data(~Option<Data>);
 
 fn generic<T>( _ : ~[(Data,T)] ) {
+    // The important detail is substituting (bool,T) for T on each
+    // recursive call so the number of representations necessary to compile
+    // this function is infinite.
     let rec : ~[(Data,(bool,T))] = ~[];
     generic( rec );
 }
 
 fn main () {
+    // Use generic<T> at least once to trigger instantiation.
     let input : ~[(Data,())] = ~[];
     generic(input);
 }
